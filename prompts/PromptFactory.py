c1f4c67b2f7a27616631c542e53d5910a3658c16
delai50 <alexdroj@hotmail.com>
2024-04-30 08:53:07 +0000

    fixed repeated prompt

--- prompts/PromptFactory.py
+++ prompts/PromptFactory.py
@@ -18,19 +18,6 @@ class PromptFactory:
                                 # Answer:
                                 """
 
-    python_agent_template = """
-                            You are an agent designed to write and execute python code to answer questions.
-                            You have access to a python REPL, which you can use to execute python code.
-                            If you get an error, debug your code and try again.
-                            Only use the output of your code to answer the question. 
-                            You might know the answer without running any code, but you should still run the code to get the answer.
-                            If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-                            Ok, now take a deep breath and do the task step by step.
-                                
-                            # Question: {}
-                            
-                            # Answer:
-                            """
 
     system_router_template = """
                             Your role is to accurately classify user queries into the most appropriate category:
